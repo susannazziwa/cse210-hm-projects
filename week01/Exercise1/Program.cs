c27d32b428825ce1d093514e4072cc5acf2e5c3a
Susan Nazziwa <nazziwalungu@gmail.com>
2025-07-04 06:53:23 +0200

    Finished week 01: C# Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write(" What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write(" What is your second name? ");
+        string second = Console.ReadLine();
+
+        Console.WriteLine($" Your name is {second}, {first} {second}");
+
     }
 }
