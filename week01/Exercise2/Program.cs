a1b6d86837de56a3e9c8fce2d4686f05d6646a26
Susan Nazziwa <nazziwalungu@gmail.com>
2025-07-06 21:39:32 +0200

    Finished assignment

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write(" Please enter your grade: ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your score is {letter}");
+
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Keep striving");
+         }
     }
+       
 }
