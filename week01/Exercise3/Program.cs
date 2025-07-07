b5b9b51bcd49ab482d415f93cd323a0304e503d8
Susan Nazziwa <nazziwalungu@gmail.com>
2025-07-07 21:21:41 +0200

    Finished Assignment

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        while (playAgain.ToLower() == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            
+        
+
+            while (guess != magicNumber)
+            {
+                Console.Write(" What is your guess? ");
+                string userInPut1 = Console.ReadLine();
+
+                bool isValid = int.TryParse(userInPut1, out guess);
+                if (!isValid)
+                {
+                    Console.WriteLine("Please enter a valid number");
+                    continue;
+                }
+
+                
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+
+                    Console.WriteLine("You guessed it !");
+
+                }
+            }
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+        }
+        Console.WriteLine("Thanks for playing");
     }
 }
